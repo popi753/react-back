b1bed4549b5d6dab56ad2991660df2385b739f9c
popi753 <luka.popi.919@gmail.com>
2023-07-11 15:42:10 +0400

    first

--- app.js
+++ app.js
@@ -35,7 +35,9 @@ app.use((req, res, next)=>{
   res.header('Access-Control-Allow-Origin', "*"),
   // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'),
   res.header('Access-Control-Allow-Headers', 'Content-Type'),
-next()})
+next()
+}
+)
 
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
 
-const axios = require('axios')
 
 
 

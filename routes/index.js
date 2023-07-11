e6c00816bbe06fa0eb0442bc5fd1b0bd9d19b22b
popi753 <luka.popi.919@gmail.com>
2023-07-11 15:16:47 +0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+const mongoose = require('mongoose');
+
+const axios = require('axios')
+
+
+
+const userSchema = new mongoose.Schema({
+  username:{
+      type: String,
+      required: true,
+      unique: true,
+  },
+  password:{
+      type:String,
+      required: true,
+  }
+})
+
+
+const mycolect = mongoose.model("mycolect", userSchema)
+
+
+router.get('/', (req, res)=> {
+  res.render("index.ejs")
+})
+
+
+router.post('/users', (req,res)=>{
+  
+      const user = new mycolect({
+        username: req.body.username,
+        password: req.body.password
+      })
+
+      user.save().then(one=>{
+          res.send(one)
+
+
+      }).catch(err => err.code==11000 ? res.send("User already exists") : res.send(err))
+
+
+
+    })
+     
+    
+
+
+
+router.get('/users', (req, res,)=> {
+  mycolect.find({}).then(data => res.send(data))
+});
+
+
+
+
+module.exports = router;

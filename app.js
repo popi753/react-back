012c0a69bbd40137a64938c22f49ab25b049d8f8
popi753 <luka.popi.919@gmail.com>
2023-11-08 22:11:49 +0400

    vercel method/headers *

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next)=>{
   res.header('Access-Control-Allow-Origin', "*"),
   res.header('Access-Control-Allow-Methods', '*'),
-  res.header('Access-Control-Allow-Headers', 'Content-Type'),
+  res.header('Access-Control-Allow-Headers', '*'),
   res.header('Access-Control-Allow-Credentials', true),
 next()
 }
